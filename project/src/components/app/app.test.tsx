647cff32134293b6299fb66c8e311e7ae1094038
Stepan Grankin <sgrankin@inbox.ru>
2023-03-06 09:44:01 +0300

    fix test

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,8 +1,12 @@
 import {render, screen} from '@testing-library/react';
 import App from './app';
 
-test('Renders app-component', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Hello, world!/i);
-  expect(textElement).toBeInTheDocument();
+test('Renders expected place cards', () => {
+  const placeCardsCount = 5;
+
+  const {container} = render(<App placeCardsCount={placeCardsCount}/>);
+
+  const placeCards = container.getElementsByClassName('place-card')
+
+  expect(placeCards.length).toBe(2);
 });
