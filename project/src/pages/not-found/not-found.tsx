d051e9de1fec5ed16c94030753d4fc0a6a068e51
Stepan Grankin <sgrankin@inbox.ru>
2023-03-09 18:16:58 +0300

    not-found component

---
+++ project/src/pages/not-found/not-found.tsx
@@ -0,0 +1,10 @@
+﻿import {Link} from "react-router-dom";
+
+function NotFound(): JSX.Element {
+  return <>
+    404 Not Found
+    <Link to='/'>Go to main page</Link>
+  </>
+}
+
+export default NotFound;
