0f07b473b5ec38c66518ef9eb162b0b877944ffb
Stepan Grankin <sgrankin@inbox.ru>
2023-03-09 18:26:25 +0300

    links to components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,24 @@
-import Main from '../../pages/Main/main';
+import Main from '../../pages/main/main';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "../../pages/login/login";
+import Property from "../../pages/property/property";
+import NotFound from "../../pages/not-found/not-found";
 
 type AppProps = {
   placeCardsCount : number;
 }
 
 function App({ placeCardsCount } : AppProps): JSX.Element {
-  return <Main placeCardsCount={placeCardsCount}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main placeCardsCount={placeCardsCount} />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/offer/:id' element={<Property />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
