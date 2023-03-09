e9458eee93968e96d70d3fc365dd59f77d6b68fe
Stepan Grankin <sgrankin@inbox.ru>
2023-03-09 18:37:20 +0300

    prettify not-found page

--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -2,10 +2,18 @@ import {Link} from 'react-router-dom';
 
 function NotFound(): JSX.Element {
   return (
-    <>
-      404 Not Found
-      <Link to='/'>Go to main page</Link>
-    </>
+    <main className="page__main page__main--property">
+      <section className="property">
+        <div className="property__container container">
+          <div className="property__wrapper">
+            <div className="property__name-wrapper">
+              <h2>404 Not Found</h2>
+              <Link to='/'>Go to main page</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
