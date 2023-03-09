5d309ab55f5f423a34ed0f037d2bb40b447d433b
Stepan Grankin <sgrankin@inbox.ru>
2023-03-09 18:30:57 +0300

    fixes

--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,10 +1,12 @@
-﻿import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 function NotFound(): JSX.Element {
-  return <>
-    404 Not Found
-    <Link to='/'>Go to main page</Link>
-  </>
+  return (
+    <>
+      404 Not Found
+      <Link to='/'>Go to main page</Link>
+    </>
+  );
 }
 
 export default NotFound;
