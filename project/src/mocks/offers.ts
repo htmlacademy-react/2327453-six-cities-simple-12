1381ad911c7374eff02fbb886a45546067e84d24
Stepan Grankin <sgrankin@inbox.ru>
2023-03-10 14:36:14 +0300

    fixed offers mock

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -1,215 +1,215 @@
-﻿import {Offer} from "../types/offer";
+import {Offer} from '../types/offer';
 
 export const offers: Offer[] = [
   {
-    "city": {
-      "name": "Cologne",
-      "location": {
-        "latitude": 50.938361,
-        "longitude": 6.959974,
-        "zoom": 13
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 50.938361,
+        longitude: 6.959974,
+        zoom: 13
       }
     },
-    "previewImage": "https://12.react.pages.academy/static/hotel/2.jpg",
-    "images": [
-      "https://12.react.pages.academy/static/hotel/12.jpg",
-      "https://12.react.pages.academy/static/hotel/18.jpg",
-      "https://12.react.pages.academy/static/hotel/1.jpg",
-      "https://12.react.pages.academy/static/hotel/20.jpg",
-      "https://12.react.pages.academy/static/hotel/15.jpg",
-      "https://12.react.pages.academy/static/hotel/11.jpg",
-      "https://12.react.pages.academy/static/hotel/6.jpg",
-      "https://12.react.pages.academy/static/hotel/14.jpg",
-      "https://12.react.pages.academy/static/hotel/2.jpg",
-      "https://12.react.pages.academy/static/hotel/9.jpg",
-      "https://12.react.pages.academy/static/hotel/10.jpg",
-      "https://12.react.pages.academy/static/hotel/3.jpg",
-      "https://12.react.pages.academy/static/hotel/19.jpg",
-      "https://12.react.pages.academy/static/hotel/8.jpg"
+    previewImage: 'https://12.react.pages.academy/static/hotel/2.jpg',
+    images: [
+      'https://12.react.pages.academy/static/hotel/12.jpg',
+      'https://12.react.pages.academy/static/hotel/18.jpg',
+      'https://12.react.pages.academy/static/hotel/1.jpg',
+      'https://12.react.pages.academy/static/hotel/20.jpg',
+      'https://12.react.pages.academy/static/hotel/15.jpg',
+      'https://12.react.pages.academy/static/hotel/11.jpg',
+      'https://12.react.pages.academy/static/hotel/6.jpg',
+      'https://12.react.pages.academy/static/hotel/14.jpg',
+      'https://12.react.pages.academy/static/hotel/2.jpg',
+      'https://12.react.pages.academy/static/hotel/9.jpg',
+      'https://12.react.pages.academy/static/hotel/10.jpg',
+      'https://12.react.pages.academy/static/hotel/3.jpg',
+      'https://12.react.pages.academy/static/hotel/19.jpg',
+      'https://12.react.pages.academy/static/hotel/8.jpg'
     ],
-    "title": "House in countryside",
-    "isPremium": true,
-    "rating": 4.6,
-    "type": "room",
-    "bedrooms": 1,
-    "maxAdults": 2,
-    "price": 153,
-    "goods": [
-      "Breakfast",
-      "Air conditioning",
-      "Washer",
-      "Laptop friendly workspace"
+    title: 'House in countryside',
+    isPremium: true,
+    rating: 4.6,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 2,
+    price: 153,
+    goods: [
+      'Breakfast',
+      'Air conditioning',
+      'Washer',
+      'Laptop friendly workspace'
     ],
-    "host": {
-      "id": 25,
-      "name": "Angelina",
-      "isPro": true,
-      "avatarUrl": "img/avatar-angelina.jpg"
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
     },
-    "description": "Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.",
-    "location": {
-      "latitude": 50.916361,
-      "longitude": 6.944974,
-      "zoom": 16
+    description: 'Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.',
+    location: {
+      latitude: 50.916361,
+      longitude: 6.944974,
+      zoom: 16
     },
-    "id": 1
+    id: 1
   },
   {
-    "city": {
-      "name": "Paris",
-      "location": {
-        "latitude": 48.85661,
-        "longitude": 2.351499,
-        "zoom": 13
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
       }
     },
-    "previewImage": "https://12.react.pages.academy/static/hotel/16.jpg",
-    "images": [
-      "https://12.react.pages.academy/static/hotel/10.jpg",
-      "https://12.react.pages.academy/static/hotel/16.jpg",
-      "https://12.react.pages.academy/static/hotel/18.jpg",
-      "https://12.react.pages.academy/static/hotel/11.jpg",
-      "https://12.react.pages.academy/static/hotel/8.jpg",
-      "https://12.react.pages.academy/static/hotel/1.jpg",
-      "https://12.react.pages.academy/static/hotel/7.jpg",
-      "https://12.react.pages.academy/static/hotel/15.jpg",
-      "https://12.react.pages.academy/static/hotel/12.jpg",
-      "https://12.react.pages.academy/static/hotel/14.jpg",
-      "https://12.react.pages.academy/static/hotel/20.jpg",
-      "https://12.react.pages.academy/static/hotel/17.jpg",
-      "https://12.react.pages.academy/static/hotel/4.jpg",
-      "https://12.react.pages.academy/static/hotel/2.jpg"
+    previewImage: 'https://12.react.pages.academy/static/hotel/16.jpg',
+    images: [
+      'https://12.react.pages.academy/static/hotel/10.jpg',
+      'https://12.react.pages.academy/static/hotel/16.jpg',
+      'https://12.react.pages.academy/static/hotel/18.jpg',
+      'https://12.react.pages.academy/static/hotel/11.jpg',
+      'https://12.react.pages.academy/static/hotel/8.jpg',
+      'https://12.react.pages.academy/static/hotel/1.jpg',
+      'https://12.react.pages.academy/static/hotel/7.jpg',
+      'https://12.react.pages.academy/static/hotel/15.jpg',
+      'https://12.react.pages.academy/static/hotel/12.jpg',
+      'https://12.react.pages.academy/static/hotel/14.jpg',
+      'https://12.react.pages.academy/static/hotel/20.jpg',
+      'https://12.react.pages.academy/static/hotel/17.jpg',
+      'https://12.react.pages.academy/static/hotel/4.jpg',
+      'https://12.react.pages.academy/static/hotel/2.jpg'
     ],
-    "title": "Penthouse, 4-5 rooms + 5 balconies",
-    "isPremium": false,
-    "rating": 4.5,
-    "type": "house",
-    "bedrooms": 3,
-    "maxAdults": 8,
-    "price": 709,
-    "goods": [
-      "Laptop friendly workspace",
-      "Baby seat",
-      "Breakfast",
-      "Air conditioning",
-      "Towels",
-      "Washer"
+    title: 'Penthouse, 4-5 rooms + 5 balconies',
+    isPremium: false,
+    rating: 4.5,
+    type: 'house',
+    bedrooms: 3,
+    maxAdults: 8,
+    price: 709,
+    goods: [
+      'Laptop friendly workspace',
+      'Baby seat',
+      'Breakfast',
+      'Air conditioning',
+      'Towels',
+      'Washer'
     ],
-    "host": {
-      "id": 25,
-      "name": "Angelina",
-      "isPro": true,
-      "avatarUrl": "img/avatar-angelina.jpg"
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
     },
-    "description": "Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.",
-    "location": {
-      "latitude": 48.858610000000006,
-      "longitude": 2.330499,
-      "zoom": 16
+    description: 'Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.',
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16
     },
-    "id": 2
+    id: 2
   },
   {
-    "city": {
-      "name": "Hamburg",
-      "location": {
-        "latitude": 53.550341,
-        "longitude": 10.000654,
-        "zoom": 13
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 53.550341,
+        longitude: 10.000654,
+        zoom: 13
       }
     },
-    "previewImage": "https://12.react.pages.academy/static/hotel/12.jpg",
-    "images": [
-      "https://12.react.pages.academy/static/hotel/7.jpg",
-      "https://12.react.pages.academy/static/hotel/6.jpg",
-      "https://12.react.pages.academy/static/hotel/17.jpg",
-      "https://12.react.pages.academy/static/hotel/11.jpg",
-      "https://12.react.pages.academy/static/hotel/3.jpg",
-      "https://12.react.pages.academy/static/hotel/14.jpg",
-      "https://12.react.pages.academy/static/hotel/8.jpg",
-      "https://12.react.pages.academy/static/hotel/18.jpg",
-      "https://12.react.pages.academy/static/hotel/15.jpg",
-      "https://12.react.pages.academy/static/hotel/12.jpg",
-      "https://12.react.pages.academy/static/hotel/13.jpg",
-      "https://12.react.pages.academy/static/hotel/2.jpg",
-      "https://12.react.pages.academy/static/hotel/5.jpg",
-      "https://12.react.pages.academy/static/hotel/10.jpg"
+    previewImage: 'https://12.react.pages.academy/static/hotel/12.jpg',
+    images: [
+      'https://12.react.pages.academy/static/hotel/7.jpg',
+      'https://12.react.pages.academy/static/hotel/6.jpg',
+      'https://12.react.pages.academy/static/hotel/17.jpg',
+      'https://12.react.pages.academy/static/hotel/11.jpg',
+      'https://12.react.pages.academy/static/hotel/3.jpg',
+      'https://12.react.pages.academy/static/hotel/14.jpg',
+      'https://12.react.pages.academy/static/hotel/8.jpg',
+      'https://12.react.pages.academy/static/hotel/18.jpg',
+      'https://12.react.pages.academy/static/hotel/15.jpg',
+      'https://12.react.pages.academy/static/hotel/12.jpg',
+      'https://12.react.pages.academy/static/hotel/13.jpg',
+      'https://12.react.pages.academy/static/hotel/2.jpg',
+      'https://12.react.pages.academy/static/hotel/5.jpg',
+      'https://12.react.pages.academy/static/hotel/10.jpg'
     ],
-    "title": "Amazing and Extremely Central Flat",
-    "isPremium": false,
-    "rating": 3.1,
-    "type": "room",
-    "bedrooms": 1,
-    "maxAdults": 1,
-    "price": 232,
-    "goods": [
-      "Washer",
-      "Laptop friendly workspace",
-      "Breakfast"
+    title: 'Amazing and Extremely Central Flat',
+    isPremium: false,
+    rating: 3.1,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 1,
+    price: 232,
+    goods: [
+      'Washer',
+      'Laptop friendly workspace',
+      'Breakfast'
     ],
-    "host": {
-      "id": 25,
-      "name": "Angelina",
-      "isPro": true,
-      "avatarUrl": "img/avatar-angelina.jpg"
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
     },
-    "description": "I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.",
-    "location": {
-      "latitude": 53.565341,
-      "longitude": 9.980654000000001,
-      "zoom": 16
+    description: 'I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.',
+    location: {
+      latitude: 53.565341,
+      longitude: 9.980654000000001,
+      zoom: 16
     },
-    "id": 3
+    id: 3
   },
   {
-    "city": {
-      "name": "Amsterdam",
-      "location": {
-        "latitude": 52.37454,
-        "longitude": 4.897976,
-        "zoom": 13
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
       }
     },
-    "previewImage": "https://12.react.pages.academy/static/hotel/12.jpg",
-    "images": [
-      "https://12.react.pages.academy/static/hotel/8.jpg",
-      "https://12.react.pages.academy/static/hotel/16.jpg",
-      "https://12.react.pages.academy/static/hotel/14.jpg",
-      "https://12.react.pages.academy/static/hotel/12.jpg",
-      "https://12.react.pages.academy/static/hotel/17.jpg",
-      "https://12.react.pages.academy/static/hotel/5.jpg",
-      "https://12.react.pages.academy/static/hotel/18.jpg",
-      "https://12.react.pages.academy/static/hotel/13.jpg",
-      "https://12.react.pages.academy/static/hotel/19.jpg",
-      "https://12.react.pages.academy/static/hotel/2.jpg",
-      "https://12.react.pages.academy/static/hotel/11.jpg",
-      "https://12.react.pages.academy/static/hotel/15.jpg",
-      "https://12.react.pages.academy/static/hotel/9.jpg",
-      "https://12.react.pages.academy/static/hotel/10.jpg"
+    previewImage: 'https://12.react.pages.academy/static/hotel/12.jpg',
+    images: [
+      'https://12.react.pages.academy/static/hotel/8.jpg',
+      'https://12.react.pages.academy/static/hotel/16.jpg',
+      'https://12.react.pages.academy/static/hotel/14.jpg',
+      'https://12.react.pages.academy/static/hotel/12.jpg',
+      'https://12.react.pages.academy/static/hotel/17.jpg',
+      'https://12.react.pages.academy/static/hotel/5.jpg',
+      'https://12.react.pages.academy/static/hotel/18.jpg',
+      'https://12.react.pages.academy/static/hotel/13.jpg',
+      'https://12.react.pages.academy/static/hotel/19.jpg',
+      'https://12.react.pages.academy/static/hotel/2.jpg',
+      'https://12.react.pages.academy/static/hotel/11.jpg',
+      'https://12.react.pages.academy/static/hotel/15.jpg',
+      'https://12.react.pages.academy/static/hotel/9.jpg',
+      'https://12.react.pages.academy/static/hotel/10.jpg'
     ],
-    "title": "Canal View Prinsengracht",
-    "isPremium": false,
-    "rating": 2.1,
-    "type": "room",
-    "bedrooms": 1,
-    "maxAdults": 1,
-    "price": 221,
-    "goods": [
-      "Laptop friendly workspace",
-      "Breakfast"
+    title: 'Canal View Prinsengracht',
+    isPremium: false,
+    rating: 2.1,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 1,
+    price: 221,
+    goods: [
+      'Laptop friendly workspace',
+      'Breakfast'
     ],
-    "host": {
-      "id": 25,
-      "name": "Angelina",
-      "isPro": true,
-      "avatarUrl": "img/avatar-angelina.jpg"
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
     },
-    "description": "Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.",
-    "location": {
-      "latitude": 52.361540000000005,
-      "longitude": 4.883976,
-      "zoom": 16
+    description: 'Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.',
+    location: {
+      latitude: 52.361540000000005,
+      longitude: 4.883976,
+      zoom: 16
     },
-    "id": 4
+    id: 4
   },
 ];
