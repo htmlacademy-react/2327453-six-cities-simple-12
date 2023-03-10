5900e5a0010830ba3c2000ef246779703e62b211
Stepan Grankin <sgrankin@inbox.ru>
2023-03-10 14:40:27 +0300

    removed redundant props

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {offers} from './mocks/offers';
 
-const AppSettings = {
-  placeCardsCount : 5,
-} as const;
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
@@ -14,7 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App
-      placeCardsCount={AppSettings.placeCardsCount}
       offers = {offers}
     />
   </React.StrictMode>,
