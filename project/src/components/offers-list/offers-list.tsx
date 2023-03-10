38f4901e76fca036c4d8e14d361d2d305b799271
Stepan Grankin <sgrankin@inbox.ru>
2023-03-10 14:49:38 +0300

    useState at offers-list component

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -1,11 +1,15 @@
 import {Offers} from '../../types/offer';
 import OfferCard from '../offer-card/offer-card';
+import {useState} from 'react';
 
 type OffersListProps = {
   offers: Offers;
 }
 
 function OffersList({offers}:OffersListProps): JSX.Element {
+
+  const [hoveredCardId, setHoveredCardId] = useState(1);
+
   return (
     <>
       {
