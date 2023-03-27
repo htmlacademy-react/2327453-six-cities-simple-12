4939483b50115fbf9c7c198972c04b58a8c0feaf
Stepan Grankin <sgrankin@inbox.ru>
2023-03-27 09:23:44 +0300

    enum AppRoute used at app component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,6 +4,7 @@ import Login from '../../pages/login/login';
 import Property from '../../pages/property/property';
 import NotFound from '../../pages/not-found/not-found';
 import {Offers} from '../../types/offer';
+import {AppRoute} from "../../const";
 
 type AppProps = {
   offers: Offers;
@@ -13,10 +14,10 @@ function App({ offers } : AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main offers={offers} />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/offer/:id' element={<Property offers={offers} />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path={AppRoute.Main} element={<Main offers={offers} />} />
+        <Route path={AppRoute.Login} element={<Login />} />
+        <Route path={AppRoute.Property} element={<Property offers={offers} />} />
+        <Route path={AppRoute.NotFound} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
