2faeee0135f16219f63ec381d391ff59efd33006
Stepan Grankin <sgrankin@inbox.ru>
2023-03-27 17:43:49 +0300

    added reviews item

---
+++ project/src/components/review-item/reviews-item.tsx
@@ -0,0 +1,33 @@
+﻿import {Review} from "../../types/review";
+import React from "react";
+
+function ReviewsItem(review: Review) : JSX.Element {
+  const rating = review.rating.getPercents();
+
+  return (
+  <>
+    <div className="reviews__user user">
+      <div className="reviews__avatar-wrapper user__avatar-wrapper">
+        <img className="reviews__avatar user__avatar" src={review.user.avatarUrl} width="54" height="54" alt="Reviews avatar" />
+      </div>
+      <span className="reviews__user-name">
+                        {review.user.name}
+                    </span>
+    </div>
+    <div className="reviews__info">
+    <div className="reviews__rating rating">
+      <div className="reviews__stars rating__stars">
+        <span style={{width: `${rating}%`}}></span>
+        <span className="visually-hidden">Rating</span>
+      </div>
+    </div>
+    <p className="reviews__text">
+      {review.comment}
+    </p>
+    <time className="reviews__time" dateTime={review.date.toDateString()}>{review.date.toDateString()}</time>
+    </div>
+  </>
+  );
+}
+
+export default ReviewsItem;
