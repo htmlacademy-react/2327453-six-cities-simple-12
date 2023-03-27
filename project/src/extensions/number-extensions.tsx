d0c25d9e501213304b66f94ecca804cbf7cff551
Stepan Grankin <sgrankin@inbox.ru>
2023-03-27 17:39:38 +0300

    added number extensions

---
+++ project/src/extensions/number-extensions.tsx
@@ -0,0 +1,12 @@
+﻿declare global {
+  interface Number {
+    getPercents(): number;
+  }
+}
+
+Number.prototype.getPercents = function (): number {
+  const thisNumber = Number(this);
+  return Math.round(thisNumber) * 20;
+};
+
+export {};
