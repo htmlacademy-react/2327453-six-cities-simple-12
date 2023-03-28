305033cd8e62aff26c27d34a2880c68fa281915d
Stepan Grankin <sgrankin@inbox.ru>
2023-03-28 17:54:15 +0300

    added NearPlacesList component

---
+++ project/src/components/near-places-list/near-places-list.tsx
@@ -0,0 +1,26 @@
+﻿import OffersList from '../offers-list/offers-list';
+import React from 'react';
+import {Offers} from '../../types/offer';
+
+type NearPlacesListProps = {
+  offers: Offers;
+  onMouseEnter(offerId: number): void;
+  onMouseLeave(): void;
+}
+
+function NearPlacesList({offers, onMouseEnter, onMouseLeave}: NearPlacesListProps): JSX.Element {
+  return (
+    <section className="near-places places">
+      <h2 className="near-places__title">Other places in the neighbourhood</h2>
+      <div className="near-places__list places__list">
+        <OffersList
+          offers={offers}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      </div>
+    </section>
+  );
+}
+
+export default NearPlacesList;
