66de06754e7f725f1e037549cfc8f7db02ef89d9
Stepan Grankin <sgrankin@inbox.ru>
2023-03-28 17:22:31 +0300

    used ReviewsList component

--- project/src/pages/property/property.tsx
+++ project/src/pages/property/property.tsx
@@ -2,14 +2,13 @@ import {Offers} from '../../types/offer';
 import {useParams} from 'react-router-dom';
 import NotFound from '../not-found/not-found';
 import OffersList from '../../components/offers-list/offers-list';
-import ReviewsForm from '../../components/reviews-form/reviews-form';
 import '../../types/string-extensions';
 import '../../types/number-extensions';
 import Map from '../../components/map/map';
 import React, {useState} from 'react';
 import {Point} from '../../types/location';
 import {Reviews} from '../../types/review';
-import ReviewsItem from '../../components/reviews-item/reviews-item';
+import ReviewsList from "../../components/reviews-list/reviews-list";
 
 const PropertySettings = {
   maxImages : 6,
@@ -124,43 +123,7 @@ function Property({offers, reviews}:PropertyProps): JSX.Element {
                 </p>
               </div>
             </div>
-            <section className="property__reviews reviews">
-              <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-              <ul className="reviews__list">
-                {
-                  reviews.map((review) => (
-                    <li className="reviews__item" key={review.id}>
-                      <ReviewsItem
-                        review={review}
-                      />
-                    </li>
-                  ))
-                }
-                <li className="reviews__item">
-                  <div className="reviews__user user">
-                    <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                      <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                    </div>
-                    <span className="reviews__user-name">
-                        Max
-                    </span>
-                  </div>
-                  <div className="reviews__info">
-                    <div className="reviews__rating rating">
-                      <div className="reviews__stars rating__stars">
-                        <span style={{width: '80%'}}></span>
-                        <span className="visually-hidden">Rating</span>
-                      </div>
-                    </div>
-                    <p className="reviews__text">
-                      A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                    </p>
-                    <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                  </div>
-                </li>
-              </ul>
-              <ReviewsForm />
-            </section>
+            <ReviewsList reviews={reviews} />
           </div>
         </div>
         <Map city={otherOffers[0].city} points={points} selectedPointId={hoveredCardId} className={'property__map'}></Map>
