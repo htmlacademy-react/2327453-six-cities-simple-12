f13c832b6494fff3e256c4acc04f42a97a6e9d82
Stepan Grankin <sgrankin@inbox.ru>
2023-04-03 09:53:17 +0300

    set className for active tab

--- project/src/components/cities-list/cities-list.tsx
+++ project/src/components/cities-list/cities-list.tsx
@@ -1,4 +1,6 @@
 ﻿import React from "react";
+import {useDispatch} from "react-redux";
+import {useAppSelector} from "../../hooks";
 
 type CitiesListProps = {
   citiesNames: string[];
@@ -6,17 +8,32 @@ type CitiesListProps = {
 
 function CitiesList(props: CitiesListProps): JSX.Element {
   const {citiesNames} = props;
+
+  const dispatch = useDispatch();
+  const currentCity = useAppSelector((state) => state.cityName);
+
+  const tabsClassName = `locations__item-link tabs__item `;
+  const activeTabClassName = 'tabs__item--active';
+
   return (
     <section className="locations container">
       <ul className="locations__list tabs__list">
         {
-          citiesNames.map((city) => (
-            <li className="locations__item" key={city}>
-              <a className="locations__item-link tabs__item tabs__item--active">
-                <span>city</span>
-              </a>
-            </li>
-          ))
+          citiesNames.map((city) =>
+          {
+            let className = tabsClassName;
+
+            if(currentCity === city)
+              className += activeTabClassName;
+
+            return (
+              <li className="locations__item" key={city}>
+                <a className={className}>
+                  <span>city</span>
+                </a>
+              </li>
+            );
+          })
         }
         <li className="locations__item">
           <a className="locations__item-link tabs__item" href="/#">
