c26ae91d25ffa358021a678177072393a44a1bdf
Stepan Grankin <sgrankin@inbox.ru>
2023-04-03 10:02:20 +0300

    fixes

--- project/src/components/cities-list/cities-list.tsx
+++ project/src/components/cities-list/cities-list.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {useDispatch} from 'react-redux';
 import {useAppSelector} from '../../hooks';
 
 type CitiesListProps = {
@@ -28,42 +27,12 @@ function CitiesList(props: CitiesListProps): JSX.Element {
             return (
               <li className="locations__item" key={city}>
                 <a className={className}>
-                  <span>city</span>
+                  <span>{city}</span>
                 </a>
               </li>
             );
           })
         }
-        <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="/#">
-            <span>Paris</span>
-          </a>
-        </li>
-        <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="/#">
-            <span>Cologne</span>
-          </a>
-        </li>
-        <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="/#">
-            <span>Brussels</span>
-          </a>
-        </li>
-        <li className="locations__item">
-          <a className="locations__item-link tabs__item tabs__item--active">
-            <span>Amsterdam</span>
-          </a>
-        </li>
-        <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="/#">
-            <span>Hamburg</span>
-          </a>
-        </li>
-        <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="/#">
-            <span>Dusseldorf</span>
-          </a>
-        </li>
       </ul>
     </section>
   );
