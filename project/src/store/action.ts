12735f6f8ec58100391b82feb8780b36191ad2f7
Stepan Grankin <sgrankin@inbox.ru>
2023-04-14 20:40:59 +0300

    added sortOffers action

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -2,3 +2,4 @@ import {createAction} from '@reduxjs/toolkit';
 
 export const changeCity = createAction<string>('changeCity');
 export const getOffers = createAction('getOffers');
+export const sortOffers = createAction<string>('sortOffers');
