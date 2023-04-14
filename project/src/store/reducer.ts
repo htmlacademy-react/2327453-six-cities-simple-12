4d5e3ab8e566336dbd8aca76ecde95f0e93dd656
Stepan Grankin <sgrankin@inbox.ru>
2023-04-14 20:41:20 +0300

    added sortOffers handler

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,15 +1,26 @@
 import {offers} from '../mocks/offers';
 import {createReducer} from '@reduxjs/toolkit';
-import {changeCity, getOffers} from './action';
-import {Sorting} from "../types/sorting";
+import {changeCity, getOffers, sortOffers} from './action';
+import {Sorting} from '../types/sorting';
+import {Offers} from '../types/offer';
 
 const defaultCity = 'Paris';
 
-const initialState = {
+type state = {
+  cityName: string;
+  offers: Offers;
+  sorting: string;
+}
+
+const initialState: state = {
   cityName: defaultCity,
   offers: offers.filter((o) => o.city.name === defaultCity),
   sorting: Sorting.popular
 };
+
+function getFilteredOffersByCurrentCity(): Offers {
+  return offers.filter((o) => o.city.name === initialState.cityName);
+}
 
 const reducer = createReducer(initialState, (builder) => {
   builder
@@ -17,7 +28,26 @@ const reducer = createReducer(initialState, (builder) => {
       state.cityName = action.payload;
     })
     .addCase(getOffers, (state) => {
-      state.offers = offers.filter((o) => o.city.name === state.cityName);
+      state.offers = getFilteredOffersByCurrentCity();
+    })
+    .addCase(sortOffers, (state, action) => {
+      state.sorting = action.payload;
+
+      switch(state.sorting)
+      {
+        case Sorting.priceLowToHigh:
+          state.offers = state.offers.sort((a,b) => a.price - b.price);
+          break;
+        case Sorting.priceHighToLow:
+          state.offers = state.offers.sort((a,b) => b.price - a.price);
+          break;
+        case Sorting.topRatedFirst:
+          state.offers = state.offers.sort((a,b) => b.rating - a.rating);
+          break;
+        default:
+          state.offers = getFilteredOffersByCurrentCity();
+          break;
+      }
     });
 });
 
