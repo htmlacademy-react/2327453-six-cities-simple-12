85ac19648bce7ded98d3cda6293ad5eb2611cc54
Stepan Grankin <sgrankin@inbox.ru>
2023-04-14 20:51:57 +0300

    show and hide sorting

--- project/src/components/sorting-variants/sorting-variants.tsx
+++ project/src/components/sorting-variants/sorting-variants.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useAppDispatch, useAppSelector} from '../../hooks';
 import {Sorting} from '../../types/sorting';
 import {sortOffers} from '../../store/action';
 
 
 function SortingVariants(): JSX.Element {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
   const dispatch = useAppDispatch();
 
   const currentSorting = useAppSelector((state) => state.sorting);
@@ -19,20 +21,25 @@ function SortingVariants(): JSX.Element {
     return classname;
   }
 
+  function sortOffersAndHideSorting (sorting:string){
+    dispatch(sortOffers(sorting));
+    setIsVisible(false);
+  }
+
   return (
     <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
-                Popular
+      <span className="places__sorting-caption">Sort by </span>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setIsVisible(!isVisible)}>
+        {currentSorting}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        <li className={setClassName(Sorting.popular)} tabIndex={0} onClick={() => dispatch(sortOffers(Sorting.popular))}>Popular</li>
-        <li className={setClassName(Sorting.priceLowToHigh)} tabIndex={0} onClick={() => dispatch(sortOffers(Sorting.priceLowToHigh))}>Price: low to high</li>
-        <li className={setClassName(Sorting.priceHighToLow)} tabIndex={0} onClick={() => dispatch(sortOffers(Sorting.priceHighToLow))}>Price: high to low</li>
-        <li className={setClassName(Sorting.topRatedFirst)} tabIndex={0} onClick={() => dispatch(sortOffers(Sorting.topRatedFirst))}>Top rated first</li>
+      <ul className={`places__options places__options--custom ${isVisible ? 'places__options--opened' : ''}`}>
+        <li className={setClassName(Sorting.popular)} tabIndex={0} onClick={() => sortOffersAndHideSorting(Sorting.popular)}>Popular</li>
+        <li className={setClassName(Sorting.priceLowToHigh)} tabIndex={0} onClick={() => sortOffersAndHideSorting(Sorting.priceLowToHigh)}>Price: low to high</li>
+        <li className={setClassName(Sorting.priceHighToLow)} tabIndex={0} onClick={() => sortOffersAndHideSorting(Sorting.priceHighToLow)}>Price: high to low</li>
+        <li className={setClassName(Sorting.topRatedFirst)} tabIndex={0} onClick={() => sortOffersAndHideSorting(Sorting.topRatedFirst)}>Top rated first</li>
       </ul>
     </form>
   );
