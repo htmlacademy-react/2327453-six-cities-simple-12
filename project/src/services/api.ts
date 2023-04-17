cf6fd53513c868b3041371cb6cf948b7d7cdeb5d
Stepan Grankin <sgrankin@inbox.ru>
2023-04-17 09:47:24 +0300

    createAPI service

---
+++ project/src/services/api.ts
@@ -0,0 +1,11 @@
+﻿import axios, {AxiosInstance} from "axios";
+
+const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+  return axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT
+  })
+}
