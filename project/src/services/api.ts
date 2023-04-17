af9f66cf6d8cf5d8de38492c092230a3a34d999b
Stepan Grankin <sgrankin@inbox.ru>
2023-04-17 09:51:30 +0300

    fixed by linter

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,11 +1,9 @@
-﻿import axios, {AxiosInstance} from "axios";
+import axios, {AxiosInstance} from 'axios';
 
 const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
 const REQUEST_TIMEOUT = 5000;
 
-export const createAPI = (): AxiosInstance => {
-  return axios.create({
-    baseURL: BACKEND_URL,
-    timeout: REQUEST_TIMEOUT
-  })
-}
+export const createAPI = (): AxiosInstance => axios.create({
+  baseURL: BACKEND_URL,
+  timeout: REQUEST_TIMEOUT
+});
