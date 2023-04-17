9d912d18749046fd53ecc822bc148545f2bbde54
Stepan Grankin <sgrankin@inbox.ru>
2023-04-17 17:45:00 +0300

    added loadOffers api action

---
+++ project/src/store/api-actions.ts
@@ -0,0 +1,19 @@
+﻿import {createAsyncThunk} from "@reduxjs/toolkit";
+import {AppDispatch, State} from "../types/state";
+import {AxiosInstance} from "axios";
+import {getOffers} from "./action";
+import {api} from "./index";
+import {Offers} from "../types/offer";
+import {APIRoute} from "../const";
+
+export const loadOffers = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'data/fetchOffers',
+  async (_arg, {dispatch, extra: api}) => {
+    const {data} = await api.get<Offers>(APIRoute.Offers);
+    dispatch(getOffers(data));
+  },
+);
