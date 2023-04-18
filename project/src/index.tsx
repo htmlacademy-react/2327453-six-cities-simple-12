c88ee3081aee788e133b9dcc660e41b185950107
Stepan Grankin <sgrankin@inbox.ru>
2023-04-18 08:30:42 +0300

    loadOffers

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import {reviews} from './mocks/reviews';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import {BrowserRouter} from 'react-router-dom';
+import {loadOffers} from './store/api-actions';
+
+store.dispatch(loadOffers());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,7 +16,7 @@ root.render(
   <React.StrictMode>
     <Provider store = {store}>
       <BrowserRouter>
-        <App reviews = {reviews} />
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
