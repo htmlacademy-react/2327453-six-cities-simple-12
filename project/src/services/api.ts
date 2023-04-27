add2e6caec10db4da7f7958d78b8449a606aab6e
Stepan Grankin <sgrankin@inbox.ru>
2023-04-27 12:53:07 +0300

    StatusCodeMapping

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,7 +1,16 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, AxiosResponse} from 'axios';
+import {StatusCodes} from "http-status-codes";
 
 const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
 const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) => StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => axios.create({
   baseURL: BACKEND_URL,
