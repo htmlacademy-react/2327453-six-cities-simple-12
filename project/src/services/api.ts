f3b8bd4dd5e752537c2c7d41c4d8d85da2665790
Stepan Grankin <sgrankin@inbox.ru>
2023-04-27 12:56:24 +0300

    used processErrorHandle

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,5 +1,6 @@
-import axios, {AxiosInstance, AxiosResponse} from 'axios';
+import axios, {AxiosError, AxiosInstance, AxiosResponse} from 'axios';
 import {StatusCodes} from "http-status-codes";
+import {processErrorHandle} from "./process-error-handle";
 
 const BACKEND_URL = 'https://12.react.pages.academy/six-cities-simple';
 const REQUEST_TIMEOUT = 5000;
@@ -12,7 +13,22 @@ const StatusCodeMapping: Record<number, boolean> = {
 
 const shouldDisplayError = (response: AxiosResponse) => StatusCodeMapping[response.status];
 
-export const createAPI = (): AxiosInstance => axios.create({
-  baseURL: BACKEND_URL,
-  timeout: REQUEST_TIMEOUT
-});
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{error: string}>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.error)
+      }
+
+      throw error;
+    }
+  );
+
+  return api;
+};
