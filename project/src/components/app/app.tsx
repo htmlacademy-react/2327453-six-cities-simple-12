3046e4f21f52d69c489363d73f99d0bda5a1934e
Stepan Grankin <sgrankin@inbox.ru>
2023-04-27 13:11:45 +0300

    used LoadingScreen component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,9 +5,18 @@ import Property from '../../pages/property/property';
 import NotFound from '../../pages/not-found/not-found';
 import {AppRoute} from '../../const';
 import {useScrollToTop} from '../../hooks/use-scroll-to-top';
+import {useAppSelector} from "../../hooks";
+import LoadingScreen from "../../pages/loading-screen/loading-screen";
 
 function App(): JSX.Element {
   useScrollToTop();
+
+  const isOffersLoadingInProgress = useAppSelector((state) => state.isOffersLoadingInProgress);
+
+  if (isOffersLoadingInProgress) {
+    return <LoadingScreen />
+  }
+
   return (
     <Routes>
       <Route path={AppRoute.Main} element={<Main />} />
