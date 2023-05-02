a2bd148291d354c7fc2f80fa3b723250f22782bb
Stepan Grankin <sgrankin@inbox.ru>
2023-05-02 16:03:35 +0300

    used authorizationStatus

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,4 +1,11 @@
+import {useAppSelector} from '../../hooks';
+import {AppRoute} from '../../const';
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 function Header(): JSX.Element {
+  const isAuthorized = useAppSelector((state) => state.authorizationStatus);
+
   return (
     <header className='header'>
       <div className='container'>
@@ -10,17 +17,28 @@ function Header(): JSX.Element {
           </div>
           <nav className='header__nav'>
             <ul className='header__nav-list'>
-              <li className='header__nav-item user'>
-                <div className='header__nav-profile'>
-                  <div className='header__avatar-wrapper user__avatar-wrapper'></div>
-                  <span className='header__user-name user__name'>Oliver.conner@gmail.com</span>
-                </div>
-              </li>
-              <li className='header__nav-item'>
-                <a className='header__nav-link' href='#'>
-                  <span className='header__signout'>Sign out</span>
-                </a>
-              </li>
+              {
+                isAuthorized ?
+                  <>
+                    <li className='header__nav-item user'>
+                      <div className='header__nav-profile'>
+                        <div className='header__avatar-wrapper user__avatar-wrapper'></div>
+                        <span className='header__user-name user__name'>Oliver.conner@gmail.com</span>
+                      </div>
+                    </li>
+                    <li className='header__nav-item'>
+                      <a className='header__nav-link' href='#'>
+                        <span className='header__signout'>Sign out</span>
+                      </a>
+                    </li>
+                  </>
+                  :
+                  <li className='header__nav-item'>
+                    <Link className='header__nav-link' to={AppRoute.Login}>
+                      <span className='header__signout'>Sign in</span>
+                    </Link>
+                  </li>
+              }
             </ul>
           </nav>
         </div>
