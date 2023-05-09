d96b4c73d0f6ceaf3000dd41b315df5d874d737b
Stepan Grankin <sgrankin@inbox.ru>
2023-05-09 18:17:48 +0300

    send credentials on login form submit

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,7 +1,17 @@
 import Header from '../../components/header/header';
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import {Credentials} from '../../types/credentials';
+import {store} from '../../store';
+import {authenticate} from '../../store/api-actions';
 
 function Login(): JSX.Element {
+  const [credentials, setCredentials] = useState<Credentials>({});
+
+  const onInputChangeHandler = ({target} : ChangeEvent<HTMLInputElement>) => {
+    const {name, value } = target;
+    setCredentials({...credentials, [name]:value});
+  };
+
   return (
     <>
       <Header />
@@ -9,14 +19,19 @@ function Login(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post"
+              onSubmit={(event: FormEvent<HTMLFormElement>) => {
+                event.preventDefault();
+                store.dispatch(authenticate(credentials));
+              }}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input className="login__input form__input" type="email" name="email" placeholder="Email" required onChange={onInputChangeHandler} />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input className="login__input form__input" type="password" name="password" placeholder="Password" required onChange={onInputChangeHandler} />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
