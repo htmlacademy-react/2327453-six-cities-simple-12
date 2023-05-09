2a328bfa69ae24eb6761b560fd1c3becea07a6c2
Stepan Grankin <sgrankin@inbox.ru>
2023-05-09 18:38:16 +0300

    added token service

---
+++ project/src/services/token.ts
@@ -0,0 +1,13 @@
+﻿const TOKEN_KEY = 'token';
+export const getToken = (): string => {
+  const token = localStorage.getItem(TOKEN_KEY);
+  return token ?? '';
+}
+
+export const saveToken = (token: string) => {
+  localStorage.setItem(TOKEN_KEY, token);
+}
+
+export const deleteToken = () => {
+  localStorage.removeItem(TOKEN_KEY);
+}
